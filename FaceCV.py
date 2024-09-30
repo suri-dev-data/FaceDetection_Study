9adabc81518fdce7d8633b52f9ac035038673957
Vinicius Suriano <viniciussurianolopesdasilva@gmail.com>
2024-09-29 21:54:45 -0300

    Add files via upload

---
+++ FaceCV.py
@@ -0,0 +1,48 @@
+import cv2
+from BBoxDetection import detect_bounding_box
+
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+
+    result, video_frame = video_capture.read()  # read frames from the video
+    if result is False:
+        break  # terminate the loop if the frame is not read successfully
+
+    faces = detect_bounding_box(video_frame)# apply the function we created to the video frame
+
+    cv2.imshow(
+        "My Face Detection Project", video_frame
+    )  # display the processed frame in a window named "My Face Detection Project"
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
+# IF YOU WANT DETECT IN AN IMAGE AND SHOW IT
+# import matplotlib.pyplot as plt
+
+
+# imagePath = 'input_image.jpg'
+
+# img = cv2.imread(imagePath)
+
+# gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# face_classifier = cv2.CascadeClassifier(
+#     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+# )
+
+# face = face_classifier.detectMultiScale(
+#     gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+# )
+
+# for (x, y, w, h) in face:
+#     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+# plt.figure(figsize=(20,10))
+# plt.imshow(img_rgb)
+# plt.axis('off')
